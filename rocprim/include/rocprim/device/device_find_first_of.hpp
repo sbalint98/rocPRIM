c61d093b99f483080c9c76173e4782bd4c582554
Cenxuan Tian <cenxuan@streamhpc.com>
2024-09-11 10:52:51 +0000

    solve conflict

--- rocprim/include/rocprim/device/device_find_first_of.hpp
+++ rocprim/include/rocprim/device/device_find_first_of.hpp
@@ -26,7 +26,6 @@
 #include "../detail/temp_storage.hpp"
 #include "config_types.hpp"
 #include "detail/ordered_block_id.hpp"
-#include "detail/ordered_block_id.hpp"
 #include "device_find_first_of_config.hpp"
 #include "device_transform.hpp"
 
@@ -41,6 +40,25 @@ BEGIN_ROCPRIM_NAMESPACE
 namespace detail
 {
 
+#define ROCPRIM_DETAIL_HIP_SYNC_AND_RETURN_ON_ERROR(name, size, start)                           \
+    do                                                                                           \
+    {                                                                                            \
+        hipError_t _error = hipGetLastError();                                                   \
+        if(_error != hipSuccess)                                                                 \
+            return _error;                                                                       \
+        if(debug_synchronous)                                                                    \
+        {                                                                                        \
+            std::cout << name << "(" << size << ")";                                             \
+            hipError_t __error = hipStreamSynchronize(stream);                                   \
+            if(__error != hipSuccess)                                                            \
+                return __error;                                                                  \
+            auto _end = std::chrono::steady_clock::now();                                        \
+            auto _d   = std::chrono::duration_cast<std::chrono::duration<double>>(_end - start); \
+            std::cout << " " << _d.count() * 1000 << " ms" << '\n';                              \
+        }                                                                                        \
+    }                                                                                            \
+    while(0)
+
 ROCPRIM_KERNEL
 static void
     init_find_first_of_kernel(size_t* output, size_t size, ordered_block_id<size_t> ordered_bid)
@@ -51,13 +69,6 @@ static void
 
 template<class Config, class InputIterator1, class InputIterator2, class BinaryFunction>
 ROCPRIM_KERNEL __launch_bounds__(device_params<Config>().kernel_config.block_size)
-void find_first_of_kernel(InputIterator1           input,
-                          InputIterator2           keys,
-                          size_t*                  output,
-                          size_t                   size,
-                          size_t                   keys_size,
-                          ordered_block_id<size_t> ordered_bid,
-                          BinaryFunction           compare_function)
 void find_first_of_kernel(InputIterator1           input,
                           InputIterator2           keys,
                           size_t*                  output,
@@ -77,14 +88,10 @@ void find_first_of_kernel(InputIterator1           input,
     using key_type = typename std::iterator_traits<InputIterator2>::value_type;
 
     const unsigned int thread_id = ::rocprim::detail::block_thread_id<0>();
-    const unsigned int thread_id = ::rocprim::detail::block_thread_id<0>();
 
     ROCPRIM_SHARED_MEMORY struct
     {
         unsigned int block_first_index;
-        size_t       global_first_index;
-
-        typename decltype(ordered_bid)::storage_type ordered_bid;
         size_t       global_first_index;
 
         typename decltype(ordered_bid)::storage_type ordered_bid;
@@ -97,20 +104,11 @@ void find_first_of_kernel(InputIterator1           input,
     syncthreads();
 
     while(true)
-    while(true)
     {
         if(thread_id == 0)
         {
             storage.global_first_index = atomic_load(output);
-            storage.global_first_index = atomic_load(output);
         }
-        const size_t block_id     = ordered_bid.get(thread_id, storage.ordered_bid);
-        const size_t block_offset = block_id * items_per_block;
-        // ordered_bid.get() calls syncthreads(), it is safe to read global_first_index
-
-        // Exit if all input has been processed or one of previous blocks has found a match
-        if(block_offset >= storage.global_first_index)
-        {
         const size_t block_id     = ordered_bid.get(thread_id, storage.ordered_bid);
         const size_t block_offset = block_id * items_per_block;
         // ordered_bid.get() calls syncthreads(), it is safe to read global_first_index
@@ -125,7 +123,6 @@ void find_first_of_kernel(InputIterator1           input,
 
         if(block_offset + items_per_block <= size)
         {
-            type items[items_per_thread];
             type items[items_per_thread];
             block_load_direct_striped<block_size>(thread_id, input + block_offset, items);
             for(size_t key_index = 0; key_index < keys_size; ++key_index)
@@ -144,8 +141,6 @@ void find_first_of_kernel(InputIterator1           input,
         else
         {
             const unsigned int valid = size - block_offset;
-
-            type items[items_per_thread];
 
             type items[items_per_thread];
             block_load_direct_striped<block_size>(thread_id, input + block_offset, items, valid);
@@ -170,13 +165,6 @@ void find_first_of_kernel(InputIterator1           input,
             atomic_min(&storage.block_first_index, thread_first_index * block_size + thread_id);
         }
         syncthreads();
-        if(storage.block_first_index != identity)
-        {
-            if(thread_id == 0)
-            {
-                atomic_min(output, block_offset + storage.block_first_index);
-            }
-            break;
         if(storage.block_first_index != identity)
         {
             if(thread_id == 0)
@@ -236,22 +224,6 @@ hipError_t find_first_of_impl(void*          temporary_storage,
             temp_storage::make_partition(&ordered_bid_storage,
                                          ordered_bid_type::get_temp_storage_layout())));
     if(result != hipSuccess || temporary_storage == nullptr)
-    using ordered_bid_type = ordered_block_id<size_t>;
-
-    // As output can be an arbitrary iterator, we need to use an intermediate buffer to do atomic
-    // operations with it
-    size_t*                    tmp_output          = nullptr;
-    ordered_bid_type::id_type* ordered_bid_storage = nullptr;
-
-    // Calculate required temporary storage
-    result = temp_storage::partition(
-        temporary_storage,
-        storage_size,
-        temp_storage::make_linear_partition(
-            temp_storage::ptr_aligned_array(&tmp_output, 1),
-            temp_storage::make_partition(&ordered_bid_storage,
-                                         ordered_bid_type::get_temp_storage_layout())));
-    if(result != hipSuccess || temporary_storage == nullptr)
     {
         return result;
     }
@@ -266,42 +238,15 @@ hipError_t find_first_of_impl(void*          temporary_storage,
     }
     init_find_first_of_kernel<<<1, 1, 0, stream>>>(tmp_output, size, ordered_bid);
     ROCPRIM_DETAIL_HIP_SYNC_AND_RETURN_ON_ERROR("init_find_first_of_kernel", 1, start);
-        return result;
-    }
 
-    auto ordered_bid = ordered_bid_type::create(ordered_bid_storage);
-
-    std::chrono::steady_clock::time_point start;
-
-    if(debug_synchronous)
-    {
-        start = std::chrono::steady_clock::now();
-    }
-    init_find_first_of_kernel<<<1, 1, 0, stream>>>(tmp_output, size, ordered_bid);
-    ROCPRIM_DETAIL_HIP_SYNC_AND_RETURN_ON_ERROR("init_find_first_of_kernel", 1, start);
-
-    if(size > 0 && keys_size > 0)
-    {
     if(size > 0 && keys_size > 0)
     {
         auto kernel = find_first_of_kernel<config, InputIterator1, InputIterator2, BinaryFunction>;
 
         const size_t shared_memory_size = 0;
 
-        const size_t shared_memory_size = 0;
-
         // Choose minimum grid size needed to achieve the highest occupancy
         int min_grid_size, max_block_size;
-        result = hipOccupancyMaxPotentialBlockSize(&min_grid_size,
-                                                   &max_block_size,
-                                                   kernel,
-                                                   shared_memory_size,
-                                                   int(block_size));
-        if(result != hipSuccess)
-        {
-            return result;
-        }
-
         result = hipOccupancyMaxPotentialBlockSize(&min_grid_size,
                                                    &max_block_size,
                                                    kernel,
@@ -319,22 +264,16 @@ hipError_t find_first_of_impl(void*          temporary_storage,
         {
             start = std::chrono::steady_clock::now();
         }
-        if(debug_synchronous)
-        {
-            start = std::chrono::steady_clock::now();
-        }
         kernel<<<num_blocks, block_size, shared_memory_size, stream>>>(input,
                                                                        keys,
                                                                        tmp_output,
                                                                        size,
                                                                        keys_size,
                                                                        ordered_bid,
-                                                                       ordered_bid,
                                                                        compare_function);
         ROCPRIM_DETAIL_HIP_SYNC_AND_RETURN_ON_ERROR("find_first_of_kernel", size, start);
     }
 
-    return transform(tmp_output, output, 1, ::rocprim::identity<void>(), stream, debug_synchronous);
     return transform(tmp_output, output, 1, ::rocprim::identity<void>(), stream, debug_synchronous);
 }
 
@@ -417,7 +356,6 @@ hipError_t find_first_of_impl(void*          temporary_storage,
 ///     temporary_storage_ptr, temporary_storage_size_bytes,
 ///     input, keys, output, size, keys_size
 /// );
-/// // output: [ 2 ]
 /// // output: [ 2 ]
 /// \endcode
 /// \endparblock
