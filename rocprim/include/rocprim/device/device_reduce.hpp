915c3015a9ab2e1134cc5f42af6a79130d115aa5
Bálint Soproni <balint@streamhpc.com>
2024-10-18 21:11:26 +0000

    Revert "Fix for Tensorflow compilation"
    This reverts commit 572b36efe3abaa441aa85bf58e90cd86a65368e1.

--- rocprim/include/rocprim/device/device_reduce.hpp
+++ rocprim/include/rocprim/device/device_reduce.hpp
@@ -41,6 +41,7 @@ namespace tensorflow
 {
 namespace functor
 {
+template<typename T>
 struct Sum;
 }
 }
@@ -105,9 +106,9 @@ template <bool WithInitialValue, class Config, class ResultType,
           class BinaryFunction, class OutputType,
           std::enable_if_t<
               !((std::is_same<BinaryFunction,
-                            ::tensorflow::functor::Sum>::value ||
+                            ::tensorflow::functor::Sum<float>>::value ||
                std::is_same<BinaryFunction, ::rocprim::plus<float>>::value) &&
-              (std::is_same<float, ResultType>::value)), bool> = true>
+              (std::is_same<float, ResultType>::value)), bool> = false>
 hipError_t block_reduce_kernel_full_nonbitwise_reproducable_int_float_launcher(
                          size_t number_of_blocks,
                          size_t block_size,
@@ -129,7 +130,7 @@ template <bool WithInitialValue, class Config, class ResultType,
           class BinaryFunction, class OutputType,
           std::enable_if_t<
               (std::is_same<BinaryFunction,
-                            ::tensorflow::functor::Sum>::value ||
+                            ::tensorflow::functor::Sum<float>>::value ||
                std::is_same<BinaryFunction, ::rocprim::plus<float>>::value) &&
               (std::is_same<float, ResultType>::value), bool>  = true>
 hipError_t block_reduce_kernel_full_nonbitwise_reproducable_int_float_launcher(
@@ -329,7 +330,7 @@ hipError_t reduce_impl(void * temporary_storage,
             start = std::chrono::steady_clock::now();
         }
         if ((std::is_same<BinaryFunction,
-                         ::tensorflow::functor::Sum>::value ||
+                         ::tensorflow::functor::Sum<float>>::value ||
             std::is_same<BinaryFunction, ::rocprim::plus<float>>::value)
             &&(std::is_same<float, result_type>::value))
                {
